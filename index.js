a1d146d38aeb049cdc17a2b2f69584a0883f6e75
MuhIqbal08 <muhiqbal2383@gmail.com>
2024-02-09 18:39:42 +0700

    final commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// eye Toggle
 let state = false;
 let passwordInput = document.getElementById("password");
 let eyeIcon = document.getElementById("eye");
@@ -18,3 +19,37 @@ function eyeToggle() {
 // passwordInput.addEventListener("click", function () {
 //     eyeIcon.classList.toggle('eye-icon-show');
 // });
+
+
+// Generate & save Password
+let passwordLength = document.getElementById('passwordLength')
+let password = document.getElementById('password')
+let saveButton = document.getElementById('saveButton')
+
+const generatePassword = (len) => {
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+    const upperAlhpabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const number = "0123456789"
+    const simbol = "!@#$%^&*()_+{}|:'`,./\[];~?><"
+
+    const data = lowerAlphabet + upperAlhpabet + number + simbol
+    let generator = '';
+    for(let index = 0; index < len; index++){
+        generator += data[~~(Math.random() * data.length)]
+    }
+    return generator;
+}
+
+const getPassword = () => {
+    const newPassword = generatePassword(passwordLength.value)
+    setTimeout(() => {
+        alert('password has been generated')
+        password.value = newPassword
+    }, 500);
+}
+
+const savePassword = () => {
+    const passwordValue = password.value
+    saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`Password saya: ${passwordValue}`))
+    saveButton.setAttribute('download', `${document.title}.txt`)
+}
